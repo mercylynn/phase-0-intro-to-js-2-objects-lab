10f43eebd7b6fba88053714e2e2d5a53aac92a28
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-15 21:09:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+let employee={
+    name:"Mercy",
+    streetAddress:"Nairobi"
+    
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return{
+        
+        ...employee,
+        name:"Sam",
+        streetAddress:"11 Broadway"
+        
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+    employee[key] = value;
+    return employee;
+
+}
+function deleteFromEmployeeByKey(employee,key){
+    
+    
+        
+        delete employee[key];
+        return{
+            ...employee,
+        }
+       
+    
+
+    
+    
+   
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+
+
+
